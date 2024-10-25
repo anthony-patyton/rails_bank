c5fb6c29c73680ac8daaa57da78ede4a928e2244
Anthony Payton <paytonanthony2020@gmail.com>
2024-10-24 21:36:43 -0600

    added devise gem and react, and fixed routes with user model

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  root "accounts#index"
+  devise_for :users
+
+  resources :accounts
+  # Render dynamic PWA files from app/views/pwa/*
+  get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+  get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+
+  # Defines the root path route ("/")
+  # root "posts#index"
+end
